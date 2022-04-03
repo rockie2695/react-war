b7b16b5ebe24c513c412d1a86c7f72b4cc39f4a1
rockie2695 <aidanchoi@arpainfinity.com>
2022-04-03 22:48:39 +0800

    add lazyload

--- src/components/AnimationRoutes.js
+++ src/components/AnimationRoutes.js
@@ -1,26 +1,36 @@
+import React, { Suspense, lazy } from "react";
 import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
-import PlayGround from "../route/PlayGround";
-import Setting from "../route/Setting";
-import React from "react";
 import "../css/AnimationRoutes.css";
+
+const PlayGround = lazy(() => import("../route/PlayGround"));
+const Setting = lazy(() => import("../route/Setting"));
 /**
  * difficult to fix react transition group bug
 Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference. Learn more about using refs safely here: https://reactjs.org/link/strict-mode-find-node
  */
+/*
+lazyload and TransitionGroup can't match together
+*/
 export default function AnimationRoutes() {
   const location = useLocation();
   return (
     <main className="relative">
-      <TransitionGroup component={null}>
-        <CSSTransition key={location.pathname} classNames="fade" timeout={1000}>
-          <Routes location={location}>
-            <Route path="/playground" element={<PlayGround />} />
-            <Route path="/setting" element={<Setting />} />
-            <Route path="*" element={<Navigate to="/playground" />} />
-          </Routes>
-        </CSSTransition>
-      </TransitionGroup>
+      <Suspense fallback={<div>Loading...</div>}>
+        <TransitionGroup component={null}>
+          <CSSTransition
+            key={location.pathname}
+            classNames="fade"
+            timeout={1000}
+          >
+            <Routes location={location}>
+              <Route path="/playground" element={<PlayGround />} />
+              <Route path="/setting" element={<Setting />} />
+              <Route path="*" element={<Navigate to="/playground" />} />
+            </Routes>
+          </CSSTransition>
+        </TransitionGroup>
+      </Suspense>
     </main>
   );
 }
