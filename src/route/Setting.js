5a8c5f0a9cda6f9f8a98eab9c2dec9e81f858aa3
rockie2695 <aidanchoi@arpainfinity.com>
2022-05-11 00:23:12 +0800

    update

--- src/route/Setting.js
+++ src/route/Setting.js
@@ -4,43 +4,52 @@ export default function Setting() {
   return (
     <div className="w-full min-h-full">
       <Header title="Setting" />
-      <div className="main-content md:p-4 p-2">
-        <fieldset className="border border-black">
-          <legend className="text-center">test</legend>
-          <div>test</div>
-        </fieldset>
-        <div>
-          <h2 className="title text-lg font-bold">Number of adding people</h2>
-          <div className="content grid grid-cols-2 md:gap-4 gap-2 md:p-4 p-2 bg-gray-300">
-            <label htmlFor="numAddPeople">Number of adding people:</label>
-            <input
-              type="number"
-              id="numAddPeople"
-              name="numAddPeople"
-              className="rounded p-1 text-center"
-              minLength={1}
-              maxLength={3}
-              min={1}
-              max={100}
-            />
+      <div className="main-content md:p-4 p-2 flex md:flex-row flex-col">
+        <div className="flex-1">
+          <div>
+            <h2 className="title text-lg font-bold">Number of adding people</h2>
+            <div className="content md:py-2 py-2 bg-gray-300 rounded-lg">
+              <div className="hover:border-gray-500 border-2 border-gray-300 md:p-2 p-2 flex rounded transition-colors">
+                <label
+                  className="flex-1 flex items-center"
+                  htmlFor="numAddPeople"
+                >
+                  Number of adding people:
+                </label>
+                <input
+                  type="number"
+                  id="numAddPeople"
+                  name="numAddPeople"
+                  className="rounded p-1 text-center flex-1"
+                  minLength={1}
+                  maxLength={3}
+                  min={1}
+                  max={100}
+                />
+              </div>
+
+              <div className="hover:border-gray-500 border-2 border-gray-300 md:p-2 p-2 flex rounded transition-colors">
+                <label
+                  className="flex-1 flex items-center"
+                  htmlFor="numAddPeople"
+                >
+                  Number of adding people:
+                </label>
+                <input
+                  type="number"
+                  id="numAddPeople"
+                  name="numAddPeople"
+                  className="rounded p-1 text-center flex-1"
+                  minLength={1}
+                  maxLength={3}
+                  min={1}
+                  max={100}
+                />
+              </div>
+            </div>
           </div>
         </div>
-        <table>
-          <tbody>
-            <tr>
-              <td>123</td>
-              <td>456</td>
-              <td>123</td>
-              <td>456</td>
-            </tr>
-            <tr>
-              <td>789</td>
-              <td>321</td>
-              <td>123</td>
-              <td>456</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="flex-1"></div>
       </div>
     </div>
   );
