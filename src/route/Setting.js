5fbb4bf731a9d18594ea42592ef9e504c9017cc9
rockie2695 <aidanchoi@arpainfinity.com>
2022-05-12 00:22:34 +0800

    update

--- src/route/Setting.js
+++ src/route/Setting.js
@@ -4,12 +4,12 @@ export default function Setting() {
   return (
     <div className="w-full min-h-full">
       <Header title="Setting" />
-      <div className="main-content md:p-4 p-2 flex md:flex-row flex-col">
-        <div className="flex-1">
+      <div className="main-content md:p-4 p-2 grid grid-cols-4">
+        <div className="md:col-span-3 col-span-4">
           <div>
             <h2 className="title text-lg font-bold">Number of adding people</h2>
             <div className="content md:py-2 py-2 bg-gray-300 rounded-lg">
-              <div className="hover:border-gray-500 border-2 border-gray-300 md:p-2 p-2 flex rounded transition-colors">
+              <div className="hover:border-gray-500 border-2 border-gray-300 md:p-2 p-2 flex rounded transition-colors min-h-[2.5rem]">
                 <label
                   className="flex-1 flex items-center"
                   htmlFor="numAddPeople"
@@ -49,7 +49,7 @@ export default function Setting() {
             </div>
           </div>
         </div>
-        <div className="flex-1"></div>
+        <div className="md:col-span-1 hidden">test</div>
       </div>
     </div>
   );
